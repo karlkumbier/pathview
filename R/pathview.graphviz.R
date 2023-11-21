c113f9aedf054ce1e61da0a32a2f06a27dd8e04a
Karl Kumbier <kkumbier@berkeley.edu>
2023-11-21 10:53:35 -0800

    tests

--- R/pathview.graphviz.R
+++ R/pathview.graphviz.R
@@ -1,53 +1,48 @@
-pathview <-
-function(
-                         gene.data=NULL,
-                         cpd.data=NULL,
-#                         xml.file=NULL,
-                         pathway.id,
-                         species = "hsa",
-                         kegg.dir=".",
-                   cpd.idtype="kegg",
-                   gene.idtype="entrez",
-                   gene.annotpkg=NULL,
-                   min.nnodes=3,#
-         
-                   kegg.native=TRUE,
-                   map.null=TRUE,
-                   expand.node=FALSE, #g
-                           split.group=FALSE, #g
+pathview_graphviz <- function(
+                              gene.data=NULL,
+                              cpd.data=NULL,
+                              #                         xml.file=NULL,
+                              pathway.id,
+                              species = "hsa",
+                              kegg.dir=".",
+                              cpd.idtype="kegg",
+                              gene.idtype="entrez",
+                              gene.annotpkg=NULL,
+                              min.nnodes=3,#
 
-                         map.symbol=TRUE,
-                         map.cpdname=TRUE, #g
-                         
-                         node.sum="sum",
-         discrete=list(gene=FALSE, cpd=FALSE),
-         limit=list(gene=1, cpd=1),
-                         bins=list(gene=10, cpd=10),
-                         both.dirs=list(gene=T, cpd=T),
-                         trans.fun = list(gene = NULL,  cpd = NULL),
-         low = list(gene = "green", cpd = "blue"),
-                         mid = list(gene = "gray", cpd = "gray"),
-                         high = list(gene = "red", cpd = "yellow"),
-         na.col="transparent",
-         
-#                         new.signature=TRUE,
-#                         plot.col.key=TRUE,
-#                         key.align="x",
-#                         key.pos="topright",
-#                         sign.pos="bottomright",#g
-                         ...){
+                              kegg.native=TRUE,
+                              map.null=TRUE,
+                              expand.node=FALSE, #g
+                              split.group=FALSE, #g
 
-#length-2 arguments check
+                              map.symbol=TRUE,
+                              map.cpdname=TRUE, #g
+
+                              node.sum="sum",
+                              discrete=list(gene=FALSE, cpd=FALSE),
+                              limit=list(gene=1, cpd=1),
+                              bins=list(gene=10, cpd=10),
+                              both.dirs=list(gene=T, cpd=T),
+                              trans.fun = list(gene = NULL,  cpd = NULL),
+                              low = list(gene = "green", cpd = "blue"),
+                              mid = list(gene = "gray", cpd = "gray"),
+                              high = list(gene = "red", cpd = "yellow"),
+                              na.col="transparent",
+                              ...){
+
+  #length-2 arguments check
 
   dtypes=!is.null(gene.data)+!is.null(cpd.data)
   cond0=dtypes==1 & is.numeric(limit) & length(limit)>1
+  
   if(cond0){
     if(limit[1]!=limit[2] & is.null(names(limit)))
       limit=list(gene=limit[1:2], cpd=limit[1:2])
   }
-  if(is.null(trans.fun)) trans.fun=list(gene = NULL,  cpd = NULL)
   
-  arg.len2=c("discrete", "limit","bins", "both.dirs", "trans.fun", "low", "mid", "high")
+  if(is.null(trans.fun)) trans.fun=list(gene = NULL,  cpd = NULL)
+
+  arg.len2 <- c("discrete", "limit", "bins", "both.dirs", "trans.fun", "low", "mid", "high")
   for(arg in arg.len2){
     obj1=eval(as.name(arg))
     if(length(obj1)==1) obj1=rep(obj1,2)
@@ -58,7 +53,7 @@ function(
     assign(arg, obj1)
   }
 
-#data.checck
+  #data.checck
   if(is.character(gene.data)){
     gd.names=gene.data
     gene.data=rep(1, length(gene.data))
@@ -81,7 +76,7 @@ function(
   }
   gene.idtype=toupper(gene.idtype)
   data(bods)
-#  data(gene.idtype.bods)
+  #  data(gene.idtype.bods)
   if(species!="ko"){
     species.data=kegg.species.code(species, na.rm=T, code.only=FALSE)
   } else {
@@ -133,7 +128,7 @@ function(
     gene.idtype="KEGG"
   }
 
-  
+
   if(is.character(cpd.data)){
     cpdd.names=cpd.data
     cpd.data=rep(1, length(cpd.data))
@@ -142,11 +137,11 @@ function(
     ncpd=length(cpd.data)
   } else if(!is.null(cpd.data)){
     if(length(dim(cpd.data))==2){
-    cpdd.names=rownames(cpd.data)
-    ncpd=nrow(cpd.data)
+      cpdd.names=rownames(cpd.data)
+      ncpd=nrow(cpd.data)
     } else if(is.numeric(cpd.data) & is.null(dim(cpd.data))){
-        cpdd.names=names(cpd.data)
-        ncpd=length(cpd.data)
+      cpdd.names=names(cpd.data)
+      ncpd=length(cpd.data)
     } else stop("wrong cpd.data format!")
   }
   if(length(grep("kegg", cpd.idtype))<1 & !is.null(cpd.data)){
@@ -159,8 +154,8 @@ function(
     cpd.data=mol.sum(cpd.data, cpd.idmap)
   }
 
-  
-#parse  
+
+  #parse  
   warn.fmt="Parsing %s file failed, please check the file!"
 
   if(length(grep(species, pathway.id))>0) {
@@ -170,13 +165,13 @@ function(
   kfiles=list.files(path=kegg.dir, pattern="[.]xml|[.]png")
   npath=length(pathway.id)
   out.list=list()#vector(mode = "list", length = npath)
-  
+
   #if(is.null(xml.file) | length(xml.file)!=npath)#custom xml and png file need to have the same names
   tfiles.xml=paste(pathway.name, "xml", sep=".")
   tfiles.png=paste(pathway.name, "png", sep=".")
   if(kegg.native) ttype=c("xml", "png") else ttype="xml"
   xml.file <- paste(kegg.dir, "/", tfiles.xml, sep = "")
-  
+
   for(i in 1:npath){
     ##  out.list=lapply(1:npath, function(i){
     if(kegg.native) tfiles=c(tfiles.xml[i],tfiles.png[i])
@@ -191,98 +186,91 @@ function(
       }
     }
 
-  if(kegg.native){
-    node.data=try(node.info(xml.file[i]), silent=T)
-    if(class(node.data)[1]=="try-error"){
-      warn.msg=sprintf(warn.fmt, xml.file[i])
-      message("Warning: ", warn.msg)
-      return(invisible(0))
+    if(kegg.native){
+      node.data=try(node.info(xml.file[i]), silent=T)
+      if(class(node.data)[1]=="try-error"){
+        warn.msg=sprintf(warn.fmt, xml.file[i])
+        message("Warning: ", warn.msg)
+        return(invisible(0))
+      }
+      node.type=c("gene","enzyme", "compound", "ortholog")
+      sel.idx=node.data$type %in% node.type
+      nna.idx=!is.na(node.data$x+node.data$y+node.data$width+node.data$height)
+      sel.idx=sel.idx & nna.idx
+      if(sum(sel.idx)<min.nnodes){
+        warn.fmt="Number of mappable nodes is below %d, %s skipped!"
+        warn.msg=sprintf(warn.fmt, min.nnodes, pathway.name[i])
+        message("Warning: ", warn.msg)
+        return(invisible(0))
+      }
+      node.data=lapply(node.data, "[", sel.idx)
+    } else {
+      gR1=try(parseKGML2Graph2(xml.file[i], genes=F, expand=expand.node, split.group=split.group), silent=T)
+      node.data=try(node.info(gR1), silent=T)
+      if(class(node.data)[1]=="try-error"){
+        warn.msg=sprintf(warn.fmt, xml.file[i])
+        message("Warning: ", warn.msg)
+        return(invisible(0))
+      }
     }
-    node.type=c("gene","enzyme", "compound", "ortholog")
-    sel.idx=node.data$type %in% node.type
-    nna.idx=!is.na(node.data$x+node.data$y+node.data$width+node.data$height)
-    sel.idx=sel.idx & nna.idx
-    if(sum(sel.idx)<min.nnodes){
-      warn.fmt="Number of mappable nodes is below %d, %s skipped!"
-      warn.msg=sprintf(warn.fmt, min.nnodes, pathway.name[i])
-      message("Warning: ", warn.msg)
-      return(invisible(0))
+
+
+    if(species=="ko") gene.node.type="ortholog" else gene.node.type="gene"
+    if((!is.null(gene.data) |map.null) & sum(node.data$type==gene.node.type)>1){
+      plot.data.gene=node.map(gene.data, node.data, node.types=gene.node.type, node.sum=node.sum, entrez.gnodes=entrez.gnodes)
+      kng=plot.data.gene$kegg.names
+      kng.char=gsub("[0-9]", "", unlist(kng))
+      if(any(kng.char>"")) entrez.gnodes=FALSE
+      if(map.symbol & species!="ko" & entrez.gnodes) {
+        if(is.na(gene.annotpkg)) {
+          warn.fmt="No annotation package for the species %s, gene symbols not mapped!"
+          warn.msg=sprintf(warn.fmt, species)
+          message("Warning: ", warn.msg)
+        } else {
+          #                browser()
+          plot.data.gene$labels=eg2id(as.character(plot.data.gene$kegg.names), category="SYMBOL", pkg.name=gene.annotpkg)[,2]
+          mapped.gnodes=rownames(plot.data.gene)
+          node.data$labels[mapped.gnodes]=plot.data.gene$labels
+        }
+      }
+      cols.ts.gene=node.color(plot.data.gene, limit$gene, bins$gene, both.dirs=both.dirs$gene, trans.fun=trans.fun$gene, discrete=discrete$gene, low=low$gene, mid=mid$gene, high=high$gene,  na.col=na.col)
+    } else plot.data.gene=cols.ts.gene=NULL
+    if((!is.null(cpd.data) | map.null) & sum(node.data$type=="compound")>1){
+      plot.data.cpd=node.map(cpd.data, node.data, node.types="compound", node.sum=node.sum)
+      if(map.cpdname & !kegg.native) { #@
+        plot.data.cpd$labels=cpdkegg2name(plot.data.cpd$labels)[,2]
+        mapped.cnodes=rownames(plot.data.cpd)
+        node.data$labels[mapped.cnodes]=plot.data.cpd$labels
+      }
+      cols.ts.cpd=node.color(plot.data.cpd, limit$cpd, bins$cpd, both.dirs=both.dirs$cpd, trans.fun=trans.fun$cpd, discrete=discrete$cpd, low=low$cpd, mid=mid$cpd, high=high$cpd, na.col=na.col)
+    } else plot.data.cpd=cols.ts.cpd=NULL
+
+    if(kegg.native) {
+      stop('kegg native not supported')
+    } else{
+      pv.pars <- keggview.graphviz(
+        plot.data.gene=plot.data.gene, 
+        cols.ts.gene=cols.ts.gene, 
+        plot.data.cpd=plot.data.cpd, 
+        cols.ts.cpd=cols.ts.cpd, 
+        node.data=node.data, 
+        path.graph=gR1, 
+        pathway.name=pathway.name[i],  
+        map.cpdname=map.cpdname, 
+        split.group=split.group, 
+        limit=limit, 
+        bins=bins, 
+        both.dirs=both.dirs, 
+        discrete=discrete, 
+        low=low, 
+        mid=mid, 
+        high=high, 
+        na.col=na.col, 
+        ...
+      )
     }
-    node.data=lapply(node.data, "[", sel.idx)
-  } else {
-    gR1=try(parseKGML2Graph2(xml.file[i], genes=F, expand=expand.node, split.group=split.group), silent=T)
-    node.data=try(node.info(gR1), silent=T)
-    if(class(node.data)[1]=="try-error"){
-      warn.msg=sprintf(warn.fmt, xml.file[i])
-      message("Warning: ", warn.msg)
-      return(invisible(0))
-    }
+
+    return(pv.pars)
   }
-
-
-  if(species=="ko") gene.node.type="ortholog" else gene.node.type="gene"
-  if((!is.null(gene.data) |map.null) & sum(node.data$type==gene.node.type)>1){
-    plot.data.gene=node.map(gene.data, node.data, node.types=gene.node.type, node.sum=node.sum, entrez.gnodes=entrez.gnodes)
-    kng=plot.data.gene$kegg.names
-    kng.char=gsub("[0-9]", "", unlist(kng))
-    if(any(kng.char>"")) entrez.gnodes=FALSE
-    if(map.symbol & species!="ko" & entrez.gnodes) {
-              if(is.na(gene.annotpkg)) {
-                warn.fmt="No annotation package for the species %s, gene symbols not mapped!"
-                warn.msg=sprintf(warn.fmt, species)
-                message("Warning: ", warn.msg)
-              } else {
-#                browser()
-                plot.data.gene$labels=eg2id(as.character(plot.data.gene$kegg.names), category="SYMBOL", pkg.name=gene.annotpkg)[,2]
-                mapped.gnodes=rownames(plot.data.gene)
-                node.data$labels[mapped.gnodes]=plot.data.gene$labels
-              }
-            }
-            cols.ts.gene=node.color(plot.data.gene, limit$gene, bins$gene, both.dirs=both.dirs$gene, trans.fun=trans.fun$gene, discrete=discrete$gene, low=low$gene, mid=mid$gene, high=high$gene,  na.col=na.col)
-          } else plot.data.gene=cols.ts.gene=NULL
-          if((!is.null(cpd.data) | map.null) & sum(node.data$type=="compound")>1){
-#          if(sum(node.data$type=="compound")>1){
-            plot.data.cpd=node.map(cpd.data, node.data, node.types="compound", node.sum=node.sum)
-            if(map.cpdname & !kegg.native) { #@
-              plot.data.cpd$labels=cpdkegg2name(plot.data.cpd$labels)[,2]
-              mapped.cnodes=rownames(plot.data.cpd)
-              node.data$labels[mapped.cnodes]=plot.data.cpd$labels
-            }
-            cols.ts.cpd=node.color(plot.data.cpd, limit$cpd, bins$cpd, both.dirs=both.dirs$cpd, trans.fun=trans.fun$cpd, discrete=discrete$cpd, low=low$cpd, mid=mid$cpd, high=high$cpd, na.col=na.col)
-          } else plot.data.cpd=cols.ts.cpd=NULL
-
-  if(kegg.native){
- pv.pars= keggview.native(plot.data.gene=plot.data.gene, cols.ts.gene=cols.ts.gene, plot.data.cpd=plot.data.cpd, cols.ts.cpd=cols.ts.cpd, node.data=node.data, pathway.name=pathway.name[i], kegg.dir=kegg.dir, limit=limit, bins=bins, both.dirs=both.dirs,discrete=discrete, low=low, mid=mid, high=high, na.col=na.col, ...)
-} else{
-  pv.pars= keggview.graph(plot.data.gene=plot.data.gene, cols.ts.gene=cols.ts.gene, plot.data.cpd=plot.data.cpd, cols.ts.cpd=cols.ts.cpd, node.data=node.data, path.graph=gR1, pathway.name=pathway.name[i],  map.cpdname=map.cpdname, split.group=split.group, limit=limit, bins=bins, both.dirs=both.dirs, discrete=discrete, low=low, mid=mid, high=high, na.col=na.col, ...)
-}
- 
-  return(pv.pars)
-
-  plot.data.gene=cbind(plot.data.gene, cols.ts.gene)
-  if(!is.null(plot.data.gene)){
-    cnames=colnames(plot.data.gene)[-(1:8)]
-    nsamp=length(cnames)/2
-    if(nsamp>1){
-      cnames[(nsamp+1):(2*nsamp)]=paste(cnames[(nsamp+1):(2*nsamp)], "col", sep=".")
-    } else cnames[2]="mol.col"
-    colnames(plot.data.gene)[-(1:8)]=cnames
-  }
-  plot.data.cpd=cbind(plot.data.cpd, cols.ts.cpd)    
-  if(!is.null(plot.data.cpd)){
-    cnames=colnames(plot.data.cpd)[-(1:8)]
-    nsamp=length(cnames)/2
-    if(nsamp>1){
-      cnames[(nsamp+1):(2*nsamp)]=paste(cnames[(nsamp+1):(2*nsamp)], "col", sep=".")
-    } else cnames[2]="mol.col"
-    colnames(plot.data.cpd)[-(1:8)]=cnames
-  }
-#  return(list(plot.data.gene=plot.data.gene, plot.data.cpd=plot.data.cpd))#out.list[[i]]=
-  out.list[[i]]=list(plot.data.gene=plot.data.gene, plot.data.cpd=plot.data.cpd)
-}
-
-  if(npath==1) out.list=out.list[[1]] else names(out.list)=pathway.name
-  
-  return(invisible(out.list))
 }
 
